c56188b628b1f50cedbd7e5595b86e0e34ab4363
JelleDekkers <jelle.dekkers@hotmail.com>
2019-05-23 15:57:04 +0200

    Moved voices to placeholder script, fixed kannon prefab structure for Adriaan

---
+++ Assets/Scripts/Interaction/InteractableTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractableTrigger : MonoBehaviour, IInteractable
+{
+    public GameObject GameObject => gameObject;
+    public Collider Collider => GetComponent<Collider>();
+
+    public bool HideAtStart => true;
+    public bool DestroyAfterInteraction => false;
+
+    [SerializeField] private UnityEvent interactionStartEvent;
+    [SerializeField] private UnityEvent interactionStopEvent;
+
+    public void OnInteractionStart()
+    {
+        interactionStartEvent.Invoke();
+    }
+
+    public void OnInteractionStop()
+    {
+        interactionStopEvent.Invoke();
+    }
+}
