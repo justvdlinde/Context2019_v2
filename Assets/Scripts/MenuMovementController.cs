5e4283c5e1de460348b27bb72c802bc811ed6ef7
JelleDekkers <jelle.dekkers@hotmail.com>
2019-05-27 11:31:00 +0200

    Fixed editor camera prefab and fixed post processing layer issues in great hall and tower scene

--- Assets/Scripts/MenuMovementController.cs
+++ Assets/Scripts/MenuMovementController.cs
@@ -5,45 +5,81 @@ using UnityEngine.EventSystems;
 
 public class MenuMovementController : MonoBehaviour
 {
-    [SerializeField] private float rotateSpeed;
-    [SerializeField] private float rotateSpeedMobile;
+    // Orbit
+    public Transform target;
+    public Vector3 tempOffset;
+    public float distance = 5.0f;
+    public float maxDistance = 20;
+    public float minDistance = .6f;
+    public float xSpeed = 200.0f;
+    public float ySpeed = 200.0f;
+    public int yMinLimit = -80;
+    public int yMaxLimit = 80;
+    public int zoomRate = 40;
+    public float panSpeed = 0.3f;
+    public float zoomDampening = 5.0f;
 
-    [SerializeField, HideInInspector] private Transform cam;
+    public float xDeg = 0.0f;
+    public float yDeg = 0.0f;
+    public float currentDistance;
+    public float desiredZoomDistance;
+    private Quaternion currentRotation;
+    private Quaternion desiredRotation;
+    private Quaternion rotation;
+    private Vector3 position;
 
-    private void OnValidate()
+    void Start()
     {
-        cam = GetComponentInChildren<Camera>().transform;
+        Init();
     }
 
-    private void Update()
+    public void Init()
     {
-        if (Input.GetMouseButton(0))
+        distance = Vector3.Distance(transform.position, target.position);
+        currentDistance = distance;
+        desiredZoomDistance = distance;
+
+        //be sure to grab the current rotations as starting points.
+        position = transform.position;
+        rotation = transform.rotation;
+        currentRotation = transform.rotation;
+        desiredRotation = transform.rotation;
+
+        xDeg = Vector3.Angle(Vector3.right, transform.right);
+        yDeg = Vector3.Angle(Vector3.up, transform.up);
+    }
+
+
+    void LateUpdate()
+    {
+        // Orbit
+        if (Input.GetMouseButton(1))
         {
-            //if(EventSystem.current.IsPointerOverGameObject()) { return; }
-
-            float horizontalDelta = 0;
-            float verticalDelta = 0;
-            float rotateSpeed = this.rotateSpeed;
-
-            // TODO: refactor this to be cleaner
-            if (Input.touchCount > 0)
-            {
-                horizontalDelta = Input.touches[0].deltaPosition.x;
-                verticalDelta = Input.touches[0].deltaPosition.y;
-                rotateSpeed = rotateSpeedMobile;
-            }
-            else
-            {
-                horizontalDelta = Input.GetAxis("Mouse X");
-                verticalDelta = Input.GetAxis("Mouse Y");
-            }
-
-            //Quaternion extraRotation = Quaternion.AngleAxis(-horizontalDelta / transform.localScale.x * rotateSpeed, Vector3.up)
-            //                            * Quaternion.AngleAxis(verticalDelta / transform.localScale.y * rotateSpeed, Vector3.right);
-
-            ////transform.rotation = transform.transform.rotation * extraRotation;
-            transform.RotateAround(transform.position, Vector3.up, horizontalDelta);
-            cam.RotateAround(transform.position, Vector3.right, verticalDelta);
+            xDeg += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+            yDeg -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+            yDeg = ClampAngle(yDeg, yMinLimit, yMaxLimit);
         }
+
+        desiredRotation = Quaternion.Euler(yDeg, xDeg, 0);
+        currentRotation = transform.rotation; 
+        rotation = Quaternion.Lerp(currentRotation, desiredRotation, Time.deltaTime * zoomDampening);
+
+        // Zoom:
+        desiredZoomDistance -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * zoomRate * Mathf.Abs(desiredZoomDistance);
+        desiredZoomDistance = Mathf.Clamp(desiredZoomDistance, minDistance, maxDistance);
+        currentDistance = Mathf.Lerp(currentDistance, desiredZoomDistance, Time.deltaTime * zoomDampening);
+        position = target.position - (rotation * Vector3.forward * currentDistance);
+
+        transform.rotation = rotation;
+        transform.position = position;
+    }
+
+    private static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
     }
 }
