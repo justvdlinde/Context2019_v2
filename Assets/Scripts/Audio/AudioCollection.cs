fe878f940fe28b2e2c327b871e546450c0ee8d0c
JelleDekkers <jelle.dekkers@hotmail.com>
2019-05-30 17:07:06 +0200

    Implemented foot step audio mechanic

---
+++ Assets/Scripts/Audio/AudioCollection.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class AudioCollection : ScriptableObject
+{
+    public AudioClip[] Clips => clips;
+    [SerializeField] private AudioClip[] clips;
+
+    public AudioClip GetRandom()
+    {
+        return clips.GetRandom();
+    }
+}
---
+++ Assets/Scripts/FootStepSystem/FootStepAudioService.cs
@@ -0,0 +1,20 @@
+﻿using ServiceLocatorNamespace;
+using UnityEngine;
+
+public class FootStepAudioService : IService
+{
+    public AudioClip[] RightFootAudioClips => rightFootAudioClips.Clips;
+    private AudioCollection rightFootAudioClips;
+
+    public AudioClip[] LeftFootAudioClips => leftFootAudioClips.Clips;
+    private AudioCollection leftFootAudioClips;
+
+    private const string RIGHT_FOOTSTEP_COLLECTION = "Footsteps Right";
+    private const string LEFT_FOOTSTEP_COLLECTION = "Footsteps Left";
+
+    public FootStepAudioService()
+    {
+        rightFootAudioClips = Resources.Load<AudioCollection>(RIGHT_FOOTSTEP_COLLECTION);
+        leftFootAudioClips = Resources.Load<AudioCollection>(LEFT_FOOTSTEP_COLLECTION);
+    }
+}
