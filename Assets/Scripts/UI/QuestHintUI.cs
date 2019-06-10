ee4338822439de30f6d26d88e2fbe931d9e24154
JelleDekkers <jelle.dekkers@hotmail.com>
2019-06-10 14:06:21 +0200

    Fixed UI toggle colors

---
+++ Assets/Scripts/UI/QuestHintUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using ServiceLocatorNamespace;
+
+public class QuestHintUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI text;
+
+    private ScenarioFlagsService flagService;
+
+    private void Start()
+    {
+        flagService = ServiceLocator.Instance.Get<ScenarioFlagsService>() as ScenarioFlagsService;
+
+    }
+
+    
+
+    private void Show()
+    {
+
+    }
+
+    private void Hide()
+    {
+
+    }
+}
