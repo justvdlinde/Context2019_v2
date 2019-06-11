d739aec9b0d7a36766af3f9034038ae1fc683239
JelleDekkers <jelle.dekkers@hotmail.com>
2019-06-11 13:14:24 +0200

    Added timeline signal for sitting/standing on the animator

--- Assets/Scripts/Actor/Actor.cs
+++ Assets/Scripts/Actor/Actor.cs
@@ -1,8 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
+[RequireComponent(typeof(Animator))]
 public class Actor : MonoBehaviour
 {
+    [SerializeField] private UnityEvent startup;
 
+    [SerializeField, HideInInspector] private Animator animator;
+
+    private void OnValidate()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Awake()
+    {
+        startup.Invoke();
+    }
+
+    public void AnimatorSetBoolTrue(string boolParameter)
+    {
+        animator.SetBool(boolParameter, true);
+    }
+
+    public void AnimatorSetBoolFalse(string boolParameter)
+    {
+        animator.SetBool(boolParameter, false);
+    }
 }
