d648b6541441bf206ed8be44d26fdc7d3327c358
JelleDekkers <jelle.dekkers@hotmail.com>
2019-06-18 13:38:42 +0200

    Added game over popup message

---
+++ Assets/Scripts/GameOverMessageHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ServiceLocatorNamespace;
+
+public class GameOverMessageHandler : MonoBehaviour
+{
+    [SerializeField, ScenarioFlag] private int finalFlag;
+    [SerializeField] private string messageHeader;
+    [SerializeField, TextArea] private string messageContent;
+
+    private ScenarioFlagsService flagService;
+    private PopupService popupService;
+
+    private void Start()
+    {
+        flagService = ServiceLocator.Instance.Get<ScenarioFlagsService>() as ScenarioFlagsService;
+        popupService = ServiceLocator.Instance.Get<PopupService>() as PopupService;
+
+        DontDestroyOnLoad(gameObject);
+        flagService.FlagAdded += OnFlagAddedEvent;
+    }
+    
+    private void OnFlagAddedEvent(ScenarioFlag flag)
+    {
+        if(flag.Hash == finalFlag)
+        {
+            ShowGameOverMessage();
+        }
+    }
+
+    private void ShowGameOverMessage()
+    {
+        popupService.InstantiatePopup(messageHeader, messageContent, new PopupButton.Settings("Ok", null));
+        flagService.FlagAdded -= OnFlagAddedEvent;
+    }
+}
