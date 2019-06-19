a32a7e41d676b86a02e97935c59e205ffb2381a8
JelleDekkers <jelle.dekkers@hotmail.com>
2019-06-19 11:11:40 +0200

    Implemented item collection system

---
+++ Assets/Scripts/Interaction/ItemCollector/Editor/ItemCollectionEditor.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ItemCollection))]
+public class ItemCollectionGroupEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if(GUILayout.Button("Find Items in current scene"))
+        {
+            ((ItemCollection)target).SetItems(FindItems().ToArray());
+        }
+    }
+
+    public HashSet<int> FindItems()
+    {
+        InteractableItem[] items = FindObjectsOfType<InteractableItem>();
+        HashSet<InteractableItem> uniqueItems = new HashSet<InteractableItem>();
+
+        Debug.Log("items found: " + items.Length);
+
+        foreach(InteractableItem item in items)
+        {
+            if(!uniqueItems.Contains(item) && item.IsViewable)
+            {
+                uniqueItems.Add(item);
+            }
+        }
+
+        HashSet<int> ids = new HashSet<int>();
+        foreach (InteractableItem item in uniqueItems)
+        {
+            if (!ids.Contains(item.ID))
+            {
+                ids.Add(item.ID);
+            }
+        }
+
+        Debug.Log("unique items found " + uniqueItems.Count);
+
+        return ids;
+    }
+}
