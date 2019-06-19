9b977cda1fdb9c61145112b4dd2823da095b22d3
JelleDekkers <jelle.dekkers@hotmail.com>
2019-06-19 12:41:05 +0200

    Added gate animation + gate open sound and adjusted sequence

---
+++ Assets/Scripts/Items/CastleGate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CastleGate : MonoBehaviour
+{
+    [SerializeField] private new Animation animation;
+    [SerializeField] private AudioSource audioSource;
+
+    public void Open()
+    {
+        animation.Play();
+        audioSource.Play();
+    }
+}
---
+++ Assets/Scripts/Items/Winge.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Winge : MonoBehaviour
+{
+    [SerializeField] private new Animation animation;
+
+    public void Play()
+    {
+        animation.Play();
+    }
+}
